6a4f29c6ec9bd9e4116639a22217bc618edad1e7
Anup-Dhakal <anup.dhakal007@gmail.com>
2025-08-13 07:39:47 +0545

    'conf'

--- projectWeather/settings.py
+++ projectWeather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-$1r9q!$l@%uhd3s$m6d5w)zwn+e8q!yos9f%zl#=5xjsu8yr(b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "projectWeather.urls"
@@ -115,8 +116,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+import os
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
